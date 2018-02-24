61b3e12ac198a431fc2a730e00b1c92ffb69f1bb
Henry Zhang <henryyihengzhang@gmail.com>
2018-02-24 16:50:45 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,15 +5,11 @@ katz_deli = []
 def line(katz_deli)
   number = 0
   name = []
-  
   katz_deli.collect  |names| 
-    
     number += 1
     name.push("Welcome, #{names}. You are number #{number} in line.")
-  
-  
+  end
   return name
-  
 end
 
 
