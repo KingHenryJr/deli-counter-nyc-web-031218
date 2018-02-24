e98f2ff98ffab3f660e2568c814182deab3a283b
Henry Zhang <henryyihengzhang@gmail.com>
2018-02-24 17:00:45 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,9 @@ def line(katz_deli)
    puts "The line is currently empty" 
  else
    current_line = "The line is currently: "
-   katz_deli.each_with_index(1) |name, i|
+   katz_deli.each_with_index(1) |name, i| 
+   
+   
  
  
  
