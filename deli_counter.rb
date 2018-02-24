a2a142479ddde5435553c35be29f51f9a035e75d
Henry Zhang <henryyihengzhang@gmail.com>
2018-02-24 16:57:13 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,21 +3,15 @@
 katz_deli = []
 
 def line(katz_deli)
-  number = 0
-  name = []
-  katz_deli.collect  |names| 
-    number += 1
-    name.push("The line is currently: #{number}. #{name}")
-  end
-  return name
+ 
+ 
+ 
 end
 
 
 def take_a_number(katz_deli, new_name)
   katz_deli.push(new_name)
   puts "Welcome, #{new_name}. You are number #{katz_deli.length} in line."
-  
-  
 end
   
   
