334321208718c36eb15c25fcd0780a7cec181f31
Henry Zhang <henryyihengzhang@gmail.com>
2018-02-24 17:15:04 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,17 +2,18 @@
 
 katz_deli = []
 
-def line(another_deli)
- if another_deli.length === 0
-   puts "The line is currently empty." 
- else
-   message = "The line is currently: "
-   another_deli.each_with_index(1) do |value, index| 
-     message += "#{index.to_i + 1}. #{value}"
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    message="The line is currently:"
+    katz_deli.each_with_index do |value, index|
+      message += " #{index.to_i+1}. #{value}"
+    end
+    puts "#{message}"
   end
-  puts message
- end
 end
+
 
 def take_a_number(katz_deli, new_name)
   katz_deli.push(new_name)
