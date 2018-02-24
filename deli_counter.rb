32a7543c3a0f05f97ddb51a55412fe1aae9a5232
Henry Zhang <henryyihengzhang@gmail.com>
2018-02-24 17:05:50 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -24,8 +24,8 @@ def now_serving(katz_deli)
   if katz_deli.length === 0 
     puts "There is nobody waiting to be served!"
   else 
-    
-  
+    ans = katz_deli.shift
+    puts "Currently serving #{ans}"
 end
   
 
